f568eb24950c666806b8881a12247a684e6f1366
zhangmin <512444693@qq.com>
2016-11-21 15:18:56 +0800

    注销解qstp功能

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"flag"
 
-	"./zm/decodeqstp"
 	"./zm/split"
 	"./zm/zmmd5"
 )
@@ -11,12 +10,12 @@ import (
 func main() {
 	useSplit := flag.Bool("s", false, "分解暴风包")
 	useMd5 := flag.Bool("m", false, "计算某个文件夹内的md5")
-	useDecode := flag.Bool("d", false, "解码qstp串")
+	//useDecode := flag.Bool("d", false, "解码qstp串")
 
 	path := flag.String("p", "", "参数 文件或文件夹路径")
 	flag.Parse()
 
-	use := *useSplit || *useMd5 || *useDecode
+	use := *useSplit || *useMd5 // || *useDecode
 	if path == nil || *path == "" || !use {
 		flag.PrintDefaults()
 		return
@@ -32,8 +31,8 @@ func main() {
 		return
 	}
 
-	if *useDecode {
-		decodeqstp.Decode(path)
-		return
-	}
+	// if *useDecode {
+	// 	decodeqstp.Decode(path)
+	// 	return
+	// }
 }
